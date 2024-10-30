5c8127bd42ad230111a11a3a516e0ad0f7c3908f
julien-35 <julien45.dubois@gmail.com>
2024-10-30 10:54:35 +0100

    mis a jour container back pour mise en prod

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,28 +3,35 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\WebpackEncoreBundle\WebpackEncoreBundle; 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
+use Nelmio\CorsBundle\NelmioCorsBundle; 
+use Symfony\Bundle\SecurityBundle\SecurityBundle; // Assurez-vous d'importer le SecurityBundle
+use Symfony\Bundle\TwigBundle\TwigBundle;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    // Ajoute la méthode registerBundles
     public function registerBundles(): iterable
     {
         $bundles = [
             new FrameworkBundle(),
-            new WebpackEncoreBundle(), // Enregistre le bundle Webpack Encore ici
-            // Ajoutez d'autres bundles nécessaires
+            new WebpackEncoreBundle(),
+            new DoctrineBundle(),
+            new DoctrineMigrationsBundle(),
+            new NelmioCorsBundle(),
+            new SecurityBundle(), 
+            new TwigBundle(),
         ];
-    
-        // Ajoutez les bundles pour l'environnement de développement
+
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            // Ajoutez d'autres bundles pour dev/test si nécessaire
+            // Ajoutez d'autres bundles si nécessaire
         }
-    
+
         return $bundles;
-    }    
+    }
 }
