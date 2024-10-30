3d6a55d28e28d24c94693fb4fedb9b7047d82f43
julien-35 <julien45.dubois@gmail.com>
2024-10-30 11:59:52 +0100

    Port Redis

--- src/Service/RedisService.php
+++ src/Service/RedisService.php
@@ -8,9 +8,16 @@ class RedisService
 {
     private Redis $redis;
 
-    public function __construct(Redis $redis) 
+    public function __construct()
     {
-        $this->redis = $redis;
+        $this->redis = new Redis();
+        $this->redis->connect(parse_url(getenv('REDIS_URL'), PHP_URL_HOST), parse_url(getenv('REDIS_URL'), PHP_URL_PORT));
+
+        // Vérifiez si l'authentification est nécessaire
+        $password = parse_url(getenv('REDIS_URL'), PHP_URL_PASS);
+        if ($password) {
+            $this->redis->auth($password);
+        }
     }
 
     public function incrementVisits(int $animalId): void
@@ -20,6 +27,6 @@ class RedisService
 
     public function getVisits(int $animalId): int
     {
-        return (int)$this->redis->get('animal_visits:' . $animalId);
+        return (int) $this->redis->get('animal_visits:' . $animalId);
     }
 }
