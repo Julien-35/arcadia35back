f42d23bec3e563c6dbc7e3c4cd3b4e83f3106881
julien-35 <julien45.dubois@gmail.com>
2024-10-30 16:29:01 +0100

    te

--- src/Service/RedisService.php
+++ src/Service/RedisService.php
@@ -3,14 +3,38 @@
 namespace App\Service;
 
 use Redis;
+use Exception;
 
 class RedisService
 {
     private Redis $redis;
 
-    public function __construct(Redis $redis) 
+    public function __construct()
     {
-        $this->redis = $redis;
+        $redisUrl = getenv('REDISCLOUD_URL');
+        if ($redisUrl === false) {
+            throw new Exception("REDISCLOUD_URL is not set.");
+        }
+
+        $this->redis = new Redis();
+        
+        // Connexion à Redis en utilisant l'URL de l'environnement
+        $urlParts = parse_url($redisUrl);
+        
+        // Vérifiez les parties de l'URL
+        if (isset($urlParts['host'], $urlParts['port'])) {
+            try {
+                $this->redis->connect($urlParts['host'], $urlParts['port']);
+                if (isset($urlParts['user'], $urlParts['pass'])) {
+                    $this->redis->auth($urlParts['user'], $urlParts['pass']);
+                }
+                echo "Connexion à Redis réussie !"; // Ajouté pour confirmation
+            } catch (Exception $e) {
+                throw new Exception("Erreur de connexion à Redis : " . $e->getMessage());
+            }
+        } else {
+            throw new Exception("Invalid REDISCLOUD_URL format.");
+        }
     }
 
     public function incrementVisits(int $animalId): void
