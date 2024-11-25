8e0a7872b3e4e47e90dbbf0eb1c5a2238443e6a6
Julien <julien45.dubois@gmail.com>
2024-11-25 10:00:19 +0100

    reset

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -74,9 +74,7 @@ public function show(): JsonResponse
 #[Route('/{id}', name: 'edit', methods: ['PUT'])]
 public function updateService(Request $request, int $id): JsonResponse
 {
-    if (!$this->isGranted('ROLE_ADMIN') && !$this->isGranted('ROLE_EMPLOYE')) {
-        return new JsonResponse(['error' => 'Unauthorized'], Response::HTTP_FORBIDDEN);
-    }
+
     // Récupérer le service par ID
     $service = $this->repository->find($id);
     
